19fb6dc25360995a06d6b89695fb3414f11371db
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-10-10 17:07:41 +0200

    box

--- src/mods/sha1/noble.ts
+++ src/mods/sha1/noble.ts
@@ -1,6 +1,7 @@
+import { Box, Copiable, Copied } from "@hazae41/box"
 import { Result } from "@hazae41/result"
 import { sha1 } from "@noble/hashes/sha1"
-import { Adapter, Copied } from "./adapter.js"
+import { Adapter } from "./adapter.js"
 import { CreateError, FinalizeError, HashError, UpdateError } from "./errors.js"
 
 export function fromNoble(): Adapter {
@@ -21,8 +22,8 @@ export function fromNoble(): Adapter {
       return Result.runAndWrapSync(() => sha1.create()).mapSync(Hasher.new).mapErrSync(CreateError.from)
     }
 
-    tryUpdate(bytes: Uint8Array) {
-      return Result.runAndWrapSync(() => this.inner.update(bytes)).clear().mapErrSync(UpdateError.from)
+    tryUpdate(bytes: Box<Copiable>) {
+      return Result.runAndWrapSync(() => this.inner.update(bytes.get().bytes)).clear().mapErrSync(UpdateError.from)
     }
 
     tryFinalize() {
@@ -31,8 +32,8 @@ export function fromNoble(): Adapter {
 
   }
 
-  function tryHash(bytes: Uint8Array) {
-    return Result.runAndWrapSync(() => sha1(bytes)).mapSync(Copied.new).mapErrSync(HashError.from)
+  function tryHash(bytes: Box<Copiable>) {
+    return Result.runAndWrapSync(() => sha1(bytes.get().bytes)).mapSync(Copied.new).mapErrSync(HashError.from)
   }
 
   return { Hasher, tryHash }
